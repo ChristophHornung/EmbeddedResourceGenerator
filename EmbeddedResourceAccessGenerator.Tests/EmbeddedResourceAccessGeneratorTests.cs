a97eda4b59af18d178e53f4e47db8ba785087fd5
Christoph Hornung <christoph.hornung@crosberg.de>
2023-01-22 09:40:21 +0100

    🏗️Made the exampler more succinct ✅Added tests

---
+++ EmbeddedResourceAccessGenerator.Tests/EmbeddedResourceAccessGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace EmbeddedResourceAccessGenerator.Tests;
+
+using Xunit;
+
+public class EmbeddedResourceAccessGeneratorTests
+{
+	[Fact]
+	public void TestTxtIsAccessible()
+	{
+		Assert.Equal("Success", EmbeddedResource.TestAssets_Test_txt.GetReader().ReadToEnd());
+	}
+
+	[Fact]
+	public void RootTestTxtIsAccessible()
+	{
+		Assert.Equal("Success", EmbeddedResource.Test_txt.GetReader().ReadToEnd());
+	}
+
+	[Fact]
+	public void SubfolderTestTxtIsAccessible()
+	{
+		Assert.Equal("Success", EmbeddedResource.TestAssets_Subfolder_Test_txt.GetReader().ReadToEnd());
+	}
+}
