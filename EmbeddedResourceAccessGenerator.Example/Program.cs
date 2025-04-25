9c2b4c88a6c3192ba051ad7f6843433558163db6
Christoph Hornung <christoph.hornung@crosberg.de>
2025-04-25 13:59:28 +0200

    ⬆️Updated nugets 📝Added example to iterate through all embedded resources

--- EmbeddedResourceAccessGenerator.Example/Program.cs
+++ EmbeddedResourceAccessGenerator.Example/Program.cs
@@ -2,13 +2,14 @@
 
 internal class Program
 {
-	static void Main(string[] args)
+	private static void Main(string[] args)
 	{
 		Program.Direct();
 		Program.Enum(EmbeddedResource.TestAsset_Test_txt);
 		Program.Enum(EmbeddedResource.TestAsset_Test2_txt);
 		Program.Enum(EmbeddedResource.RootTest_txt);
 		Program.EnumExtension(EmbeddedResource.RootTest_txt);
+		Program.ForEachEmbeddedResource();
 	}
 
 	private static void Direct()
@@ -30,5 +31,15 @@ internal class Program
 		using Stream s = resource.GetStream();
 		using StreamReader sr = new StreamReader(s);
 		Console.WriteLine(sr.ReadToEnd());
+	}
+
+	private static void ForEachEmbeddedResource()
+	{
+		foreach (EmbeddedResource resource in System.Enum.GetValues<EmbeddedResource>())
+		{
+			using Stream s = resource.GetStream();
+			using StreamReader sr = new StreamReader(s);
+			Console.WriteLine(sr.ReadToEnd());
+		}
 	}
 }
